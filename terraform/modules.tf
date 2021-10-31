6e4bbb44831a70b14c9c44cb72bc5fa559f3a2c5
Eric van Wijk <eric@van-wijk.com>
2021-10-31 10:47:23 +0100

    Add provisioner data (#2)
    * Terraform 1.0.10
    
    * Add provisioner data as tags

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -13,7 +13,7 @@ module storage {
   source                       = "./modules/storage"
 
   admin_ips                    = local.admin_cidr_ranges
-  data_owner_object_id         = local.automation_object_id
+  data_owner_object_id         = data.azurerm_client_config.current.object_id
   location                     = var.location
   log_analytics_workspace_id   = azurerm_log_analytics_workspace.monitor.id
   resource_group_name          = azurerm_resource_group.rg.name
