e2d37745262b5de355daee9faa2325f53d031239
Eric van Wijk <eric@van-wijk.com>
2022-07-22 14:47:09 +0200

    Add role assignment dependency

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -39,8 +39,11 @@ resource azurerm_storage_container app_storage_container {
   storage_account_name         = azurerm_storage_account.app_storage.name
   container_access_type        = "private"
 
-  # Creating FW rules prior to accessing storage container will fail if Terraform is run from the same Azure region
-  # depends_on                   = [azurerm_storage_account_network_rules.app_storage]
+  depends_on                   = [
+    # Creating FW rules prior to accessing storage container will fail if Terraform is run from the same Azure region
+    # azurerm_storage_account_network_rules.app_storage
+    azurerm_role_assignment.tf_data_owner
+  ]
 }
 resource azurerm_storage_blob app_storage_blob_sample {
   name                         = "sample.txt"
